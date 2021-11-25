256c50108196aaccef5ec90fd8f3691f61d350cd
AhmiJk <razaa8637@gmail.com>
2021-11-26 02:48:17 +0500

    i love you

---
+++ assigment 4.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aaa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 22 / 8;
+            Console.WriteLine(n);
+            Console.ReadLine();
+
+            int a = 99 / 8 + 21 / 11 * 7;
+            Console.WriteLine(a);
+            Console.ReadLine();
+
+            double c = 4, d = 3, p = 8, q = 2;
+            double t = q / c - p / d;
+            Console.WriteLine(t);
+            Console.ReadLine();
+
+
+        }
+    }
+}
